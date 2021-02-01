bdcb8b681880720417bc4e5ef9dcf01f319dcec0
Stefan Stanchev <stanchev89@gmail.com>
2021-02-01 22:49:30 +0200

    change db origins

--- Rest-api/config/config.js
+++ Rest-api/config/config.js
@@ -6,7 +6,7 @@ const config = {
 		port: process.env.PORT || 3000,
 		// dbURL: "mongodb://localhost:27017/quiz",
 		dbURL:"mongodb+srv://stanchev89:stanchev89@cluster0.qyezc.gcp.mongodb.net/quizDB-dump?retryWrites=true&w=majority",
-		origin: ["https://quiz.stanchev89.vercel.app/","http://localhost:4200","http://localhost:5555","https://quiz-git-main.stanchev89.vercel.app" ]
+		origin: ["https://quiz-px8w43hb1.vercel.app/","http://localhost:4200","http://localhost:5555","https://quiz-git-main.stanchev89.vercel.app" ]
 	},
 	production: {
 		port: process.env.PORT || 3000,
